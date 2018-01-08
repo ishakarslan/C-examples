89140a8552fd352a161606e54fba7804f5a0654c
ishakarslan <ishak.ars@gmail.com>
2018-01-08 16:54:33 +0300

    Create ip_to_binary.c

---
+++ ip_to_binary.c
@@ -0,0 +1,48 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int binary_conv(int);
+
+
+int main(void) {
+
+    	int num, bin, a;
+    	char  *str =  malloc(sizeof(char) * 16), *str2;
+    	unsigned char value[4] = {0};
+    	size_t index = 0;
+
+	printf("Enter an IP Address: \n");
+	scanf("%16s", str);
+
+    	str2 = str;
+    	while (*str) {
+        	if (isdigit((unsigned char)*str)) {
+            	value[index] *= 10;
+            	value[index] += *str - '0';
+        } else {
+         	index++;
+        }
+        	str++;
+    	}
+
+	printf("\nBinary Conv is :\n\n", str);
+
+	for (num = 0; num < 4; ++num){
+		bin = binary_conv(value[num]);
+	printf("%08d ", bin);
+  	}
+
+	printf("\n");
+	return 0;
+}
+
+
+int binary_conv(int num)
+{
+    	if (num == 0){
+        	return 0;
+    	}else{
+        	return (num % 2) + 10 * binary_conv(num / 2);
+    	}
+}
