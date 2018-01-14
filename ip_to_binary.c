c346892d9457da3971ff12c4de6c0863bec61201
ishakarslan <ishak.ars@gmail.com>
2018-01-14 16:53:36 +0300

    Update ip_to_binary.c

--- ip_to_binary.c
+++ ip_to_binary.c
@@ -1,44 +1,61 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <error.h>
+#include <arpa/inet.h>
 
 #define MAX_8BIT 255
 
+char isValidIpAddress(char *ipAddress);
 int binary_conv(int);
 
 int main(void) {
 
-    	int num, bin;
+    	int num, bin, k;
     	char  *str =  malloc(sizeof(char) * 16), *str2;
     	int value[4] = {0};
     	size_t index = 0;
 
-	printf("Enter an IP Address: \n");
+	printf("\nEnter an IP Address: \n");
 	scanf("%16s", str);
 
     	str2 = str;
+
+	if(!isValidIpAddress(str2)){
+		printf("Please enter a valid IP Address ...\n");
+		exit(EXIT_FAILURE);
+	}
+
     	while (*str) {
         	if (isdigit((unsigned char)*str)) {
             	value[index] *= 10;
             	value[index] += *str - '0';
-        } else {
-         	index++;
-        }
-        	str++;
+        	} else {
+         		index++;
+        	}
+       		str++;
     	}
 
-	printf("\nBinary Conv is :\n\n", str);
+	printf("\nBinary format is: \n");
 
 	for (num = 0; num < 4; ++num){
-		if(value[num] > MAX_8BIT){
-			printf("Invalid IP Address"); break;
-		} else {
 		bin = binary_conv(value[num]);
-	printf("%08d ", bin);
-  		}
+		printf("%08d ", bin);
 	}
 
-	printf("\n");
+      	printf("\n");
+	printf("\nHexadecimal format is: \n");
+	printf("0x");
+
+	for (num = 0; num < 4; ++num){
+		printf("%02x", value[num]);
+	}
+
+
+
+	printf("\n\n");
+
 	return 0;
 }
 
@@ -50,4 +67,11 @@ int binary_conv(int num)
     	} else {
         	return (num % 2) + 10 * binary_conv(num / 2);
     	}
+}
+
+char isValidIpAddress(char *ipAddress)
+{
+    	struct sockaddr_in sa;
+    	int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    	return result != 0;
 }
